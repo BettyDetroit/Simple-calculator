74c8b03465016dc4350e4c1e49f4a2698b5723c0
BettyDetroit <caubelnelly@gmail.com>
2022-10-04 10:04:03 +0200

    exo sent to repo

---
+++ script.js
@@ -0,0 +1,12 @@
+const firstValue = 1;
+//création de la variable firstValue
+const operator = "+";
+//création de la variable operator
+const secondValue = 2;
+//création de la variable secondValue
+console.log(firstValue);
+//affichage via la console de firstValue
+console.log(operator);
+//affichage via la console de operator
+console.log(secondValue);
+//affichage via la console de secondValue
